815b8be710939a2a3d196600dabea769f82c11eb
RhyGuy <rhyanv1@gmail.com>
2021-11-21 19:23:54 -0500

    initial commit

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import 'hardhat/console.sol';
+
+contract MyEpicNFT is ERC721URIStorage{
+  // OpenZeppelin: Keep track of tokens
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  // Pass name of token and it's symbol 
+  constructor() ERC721 ("TurntableNFT", "DECKS"){
+      console.log('This is my new contract!');
+  }
+
+  // User makes a NFT using this function...
+  function mintNFT() public {
+    // Get current token Id, starting at 0
+    uint256 newTokenId = _tokenIds.current();
+
+    // Mint NFT and send to user who uses 'msg.sender' 
+    _safeMint(msg.sender, newTokenId);
+
+    // Set NFT data
+    _setTokenURI(newTokenId, "https://jsonkeeper.com/b/2O6I");
+
+    // Increment NFT count for the next NFT
+    _tokenIds.increment();
+
+    console.log("An NFT w/ ID %s has been minted to %s", newTokenId, msg.sender);
+
+  }
+}
---
+++ scripts/run.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+
+const main = async () => {
+  // Compile Contract.
+  const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+  // Spin up local blockckchain
+  const nftContract = await nftContractFactory.deploy();
+  // Contract mined and deployed on mock blockchain
+  await nftContract.deployed();
+
+  console.log("Contract Deployed: ", nftContract.address);
+
+  // Call the NFT funtion
+  let txn = await nftContract.mintNFT();
+  // Wait for it to be mined...
+  await txn.wait();
+
+
+  // Make another one...
+  txn = await nftContract.mintNFT();
+  await txn.wait();
+};
+
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0)
+  } catch(error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+
+runMain();
